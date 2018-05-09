78f7841f79a95d0df4baf4fb0118e65711c972b4
Van Phan <vanthuyphan@gmail.com>
2018-05-09 13:20:10 -0500

    fix port again

--- config/env/development.js
+++ config/env/development.js
@@ -2,5 +2,5 @@ export default {
   env: 'development',
   jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
   db: 'mongodb://localhost/native-pro-development',
-  port: 8081
+  port: 80
 };
--- config/env/production.js
+++ config/env/production.js
@@ -2,5 +2,5 @@ export default {
   env: 'production',
   jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
   db: 'mongodb://localhost/nsp-production',
-  port: 8081
+  port: 80
 };
